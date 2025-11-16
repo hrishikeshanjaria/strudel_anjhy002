f063ca46f8c8aad81e9354c24d1229b498d81b48
Hrishikesh3131 <anjhy002@mymail.unisa.edu.au>
2025-11-17 08:12:09 +1030

    Updated bootstrap styling to place things in a grid

--- src/components/DJControls.jsx
+++ src/components/DJControls.jsx
@@ -36,38 +36,24 @@ function DJControls({ s1, setS1, d1, setD1, d2, setD2, onProcess, setCpm, cpm, s
     return (
         <>
            
-            <div className="container text-center">
+            <div className="row container text-center">
+                <div className="col-8">
+                
                 <div className="row">
-                    <div className="col-3">
+                    <div className="col">
                         <div className="input-group mb-3">
                             <span className="input-group-text" id="cpm_label">cpm</span>
                             <input type="text" className="form-control" id="cpm_text_input" placeholder="120" value={cpm} aria-label="cpm" aria-describedby="cpm_label" onChange={(e) => { setCpm(e.target.value); onProcess(); }} disabled />
                             <input type="range" className="form-range" min="100" max="160" step="1" id="volume_range" value={cpm} onChange={(e) => { setCpm(e.target.value); onProcess(); }} />
                         </div>
                     </div>
-                    <div className="col-3">
+                    <div className="col">
                         <span className="input-group-text" id="volume_label">Main Volume</span>
                         <input type="range" className="form-range" min="0" max="1" step="0.01" id="volume_range" value={volume} onChange={(e) => setVolume(e.target.value, "mVol")} />
                     </div>
-                    <div className="col-3">
-                        {/*<span className="input-group-text">Coming Soon</span>*/}
-                        <button className="btn btn-primary w-100" onClick={saveJSON}>
-                            Save JSON
-                        </button>
-                    </div>
-                    <div className="col-3">
-                        {/*<span className="input-group-text">Coming Soon</span>*/}
-                        <button className="btn btn-danger w-100" onClick={loadJSON}>
-                            Load JSON
-                        </button>
-                    </div>
-                    
-
-                </div>            
-
-
+                </div>
                 <div className="row justify-content-center">
-                    <div className="col-2 d-flex flex-column align-items-center">
+                    <div className="col d-flex flex-column align-items-center">
                         <div className="form-check mb-2">
                             <input className="form-check-input" type="checkbox" id="s1" checked={s1} onChange={() => handleToggle(setS1, s1)} />
                             <label className="form-check-label fs-2" htmlFor="s1">
@@ -77,9 +63,9 @@ function DJControls({ s1, setS1, d1, setD1, d2, setD2, onProcess, setCpm, cpm, s
                         <input type="range" className="form-range vertical-slider" min="1" max="500" step="10" value={s1Vol} onChange={(e) => handleVolume(e.target.value, sets1Vol)} />
                     </div>
 
-                    <div className="col-2 d-flex flex-column align-items-center">
+                    <div className="col d-flex flex-column align-items-center">
                         <div className="form-check mb-2">
-                            <input className="form-check-input" type="checkbox" id="d1" checked={d1} onChange={() => handleToggle(setD1, d1)}/>
+                            <input className="form-check-input" type="checkbox" id="d1" checked={d1} onChange={() => handleToggle(setD1, d1)} />
                             <label className="form-check-label fs-2" htmlFor="d1">
                                 🥁
                             </label>
@@ -87,7 +73,7 @@ function DJControls({ s1, setS1, d1, setD1, d2, setD2, onProcess, setCpm, cpm, s
                         <input type="range" className="form-range vertical-slider" min="0" max="1" step="0.01" value={d1Vol} onChange={(e) => handleVolume(e.target.value, setd1Vol)} />
                     </div>
 
-                    <div className="col-2 d-flex flex-column align-items-center">
+                    <div className="col d-flex flex-column align-items-center">
                         <div className="form-check mb-2">
                             <input className="form-check-input" type="checkbox" id="d2" checked={d2} onChange={() => handleToggle(setD2, d2)} />
                             <label className="form-check-label fs-2" htmlFor="d2">
@@ -96,11 +82,32 @@ function DJControls({ s1, setS1, d1, setD1, d2, setD2, onProcess, setCpm, cpm, s
                         </div>
                         <input type="range" className="form-range vertical-slider" min="0" max="1" step="0.01" value={d2Vol} onChange={(e) => handleVolume(e.target.value, setd2Vol)} />
                     </div>
-                    <div className="col-4 ms-2">
-                        <label id="label" htmlFor="settings" className="form-label">Settings</label>
+                    
+                </div>
+                </div>
+                <div className="card col-3 ms-5">
+                    <div className="row mt-3">
+                        <div className="col">
+                            {/*<span className="input-group-text">Coming Soon</span>*/}
+                            <button className="btn btn-primary w-100" onClick={saveJSON}>
+                                Save Settings
+                            </button>
+                        </div>
+                        <div className="col mt-2">
+                            {/*<span className="input-group-text">Coming Soon</span>*/}
+                            <button className="btn btn-danger w-100" onClick={loadJSON}>
+                                Load Settings
+                            </button>
+                        </div>
+                    </div>
+                
+                    <div className="row card-body">
+                        {/*<label id="label" htmlFor="settings" className="form-label" style={{ color:"#ffffff"}}>Settings</label>*/}
                         <textarea className="form-control" id="settings" rows="6" placeholder="JSON will appear here..." value={jsonText} onChange={(e) => setJsonText(e.target.value)} ></textarea>
                     </div>
                 </div>
+
+                
             </div>
 
         </>
