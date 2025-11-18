68784e72966a973342da7ed688edf57093d33d19
Hrishikesh3131 <anjhy002@mymail.unisa.edu.au>
2025-11-18 16:20:31 +1030

    commented procbuttons and preprocessTextArea

--- src/components/PreprocessTextarea.jsx
+++ src/components/PreprocessTextarea.jsx
@@ -1,4 +1,8 @@
 ﻿import "./PreprocessTextarea.css";
+
+// This component is responsible for preprocesstextarea field which is used to put the songtext to
+// play by preproc and preproc and play.It has the default value coming from app.js which is actually
+// from tunes.js which we can change live and then play any other strudel song as well.
 function PreprocessTextarea({ defaultValue, onChange }) {
     return (
         <>
--- src/components/ProcButtons.jsx
+++ src/components/ProcButtons.jsx
@@ -1,4 +1,7 @@
 ﻿import "./ProcButtons.css";
+// this file is responsible for the "proc" and "proc and play" feature, here the procbuttons take the onpreprocess and onprocandplay features from
+// app.js and uses it in the button onClick to process the songText and evaluate strudel code when we do proc&play or just put 
+// the code in the repl when we do preprocess. This component is simple but useful if we want to make live changes.
 function ProcButtons({ onPreprocess, onProcAndPlay, songText }) {
     return (
         <>
